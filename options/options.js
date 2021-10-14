e02cab159fe826a7ad3db353b1b43c928a1681d8
Crowdbotics <team@crowdbotics.com>
2021-10-14 03:39:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "silent_lab_31408",
+  url: "https://silent_lab_31408.botics.co",
+  api: "https://silent_lab_31408.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
